ebae90203e3fd438928bdaf4e03e63e8d2e42fb3
FredJDean <49915591+FredJDean@users.noreply.github.com>
2025-02-27 20:30:55 +0800

    Delete AlphaEdit directory

--- AlphaEdit/__init__.py
+++
@@ -1,3 +0,0 @@
-from .AlphaEdit_main import AlphaEditHyperParams, apply_AlphaEdit_to_model
-from .build_graph import build_graph_from_triples
-from .gnn import GNN
--- AlphaEdit/gnn/__init__.py
+++
@@ -1,1 +0,0 @@
-from .rrgcn import GNN
